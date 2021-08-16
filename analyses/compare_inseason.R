f7d939fdd9b9a3ab7dbe2214a91cee38d3a90a54
Bryce Mecum <petridish@gmail.com>
2021-08-15 21:54:41 -0800

    Use correct label

--- analyses/compare_inseason.R
+++ analyses/compare_inseason.R
@@ -16,7 +16,7 @@ pilotsonar <- read_csv("data/pilotsonar.csv")
 # Munge
 beset$source <- "LYTFSET"
 bedrift$source <- "LYTFDRIFT"
-pilotsonar$source <- "PILOTPLUS3D"
+pilotsonar$source <- "PILOMINUS3D"
 
 # Offset pilotsonar by 3 days to account for travel time
 pilotsonar$day <- pilotsonar$day - 3
